47f0bbe60251572a666d396563119fcea9e86e3b
DIARY7 <diaryhakeur@gmail.com>
2024-06-04 09:29:53 +0300

    retourne modelView

---
+++ src/mg/itu/prom16/utilitaire/ModelView.java
@@ -0,0 +1,27 @@
+package mg.itu.prom16.utilitaire;
+
+import java.util.HashMap;
+
+public class ModelView {
+    String url;
+    HashMap<String,Object> data;
+    
+    public ModelView(){
+        data = new HashMap<>();
+    }
+    public void addObject(String cle,Object object){
+        this.data.put(cle,object);   
+    }
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    public HashMap<String, Object> getData() {
+        return data;
+    }
+    public void setData(HashMap<String, Object> data) {
+        this.data = data;
+    }
+}
